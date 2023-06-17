07147a03afe5f9f9a0c36cbc8fac9ab123066464
ngoctao <ngoctao.it@gmail.com>
2023-06-18 00:30:43 +0700

    [UI] Add fonts for ui game

---
+++ VrillarWorld_U3D/Assets/Scripts/UI/PlayUI.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using StormStudio.Common.UI;
+
+namespace StormStudio.UI
+{
+    public class PlayUI : UIController
+    {
+        
+    }
+}
